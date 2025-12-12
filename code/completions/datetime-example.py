9b875baab026bac0ad4adbaeb7616a307f7946c4
Walt Ritscher <waltritscher@outlook.com>
2025-12-12 12:54:18 -0800

    add completion starter examples

---
+++ code/completions/datetime-example.py
@@ -0,0 +1,4 @@
+  # write a function that filters a list of integers
+	# by a given threshold and returns the filtered list.
+		
+
---
+++ code/completions/filter_numbers.py
@@ -0,0 +1,10 @@
+  # write a function that filters a list of integers
+	# by a given threshold and returns the filtered list.
+
+
+  # return a list of unique items, filtered to only even numbers,
+  # sorted in ascending order
+
+
+  # return a dictionary with the min, max, average,
+  # and count of the numbers in the list
