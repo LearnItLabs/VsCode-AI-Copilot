19d100cf0c3ddf3e04fac750891d7d3e720489e6
Walt Ritscher <waltritscher@outlook.com>
2025-12-29 17:10:44 -0800

    fix typo

--- code/slash-commands/dotnet-build-fix/Broken.cs
+++ code/slash-commands/dotnet-build-fix/Broken.cs
@@ -8,7 +8,7 @@ namespace ErrorZone.BuildErrors;
 internal class Broken {
 	// Error 1: Cannot implicitly convert Task<int> to int
 
-	//} 
+ 
 	internal void Show() {
 		int result = Multiply(6, 7);
 		Console.WriteLine($"Result: {result}");
